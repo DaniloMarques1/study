75b5ee3f89a30bcbe30b4b3bab90d3b1b387e87b
Danilo Marques <danilomarques20@hotmail.com>
2019-09-07 22:24:35 -0300

    Ajuste no endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // http://localhost:5000
 
 const api = axios.create({
-    baseURL: 'http://danilomarques.pythonanywhere.com'
+    baseURL: 'https://danilomarques.pythonanywhere.com'
 });
 
 export default api;
